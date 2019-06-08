cb4dcc6c1a0217eea7718ca07b4433fecf708e58
JuliaKrotova <julia9867@mail.ru>
2019-06-08 14:57:17 +0700

    исправление ошибок тревиса

--- js/stat.js
+++ js/stat.js
@@ -14,12 +14,12 @@ var TEXT_Y = CLOUD_Y + CLOUD_HEIGHT - GAP - TEXT_HEIGHT;
 
 var BAR_HEIGHT = 150 - TEXT_GAP;
 
-var renderCloud = function(ctx, x, y, color) {
+var renderCloud = function (ctx, x, y, color) {
   ctx.fillStyle = color;
   ctx.fillRect(x, y, CLOUD_WIDTH, CLOUD_HEIGHT);
 };
 
-var getMaxElement = function(arr) {
+var getMaxElement = function (arr) {
   var maxElement = arr[0];
 
   for (var i = 0; i < arr.length; i++) {
@@ -30,7 +30,7 @@ var getMaxElement = function(arr) {
   return maxElement;
 };
 
-var renderMessage = function(ctx) {
+var renderMessage = function (ctx) {
   ctx.fillStyle = '#000';
   ctx.font = '16px PT Mono';
   ctx.textBaseline = 'hanging';
@@ -38,7 +38,7 @@ var renderMessage = function(ctx) {
   ctx.fillText('Список результатов:', CLOUD_X + GAP, CLOUD_Y + GAP + TEXT_HEIGHT);
 };
 
-var renderBarChart = function(ctx, names, times) {
+var renderBarChart = function (ctx, names, times) {
   var maxTime = getMaxElement(times);
   for (var i = 0; i < names.length; i++) {
     ctx.fillStyle = '#000';
@@ -53,9 +53,9 @@ var renderBarChart = function(ctx, names, times) {
     ctx.fillStyle = '#000';
     ctx.fillText(Math.round(times[i]), CLOUD_X + BAR_GAP + i * (BAR_WIDTH + BAR_GAP), TEXT_Y - playerBarHeight - TEXT_HEIGHT);
   }
-}
+};
 
-window.renderStatistics = function(ctx, names, times) {
+window.renderStatistics = function (ctx, names, times) {
   renderCloud(ctx, CLOUD_X + SHADOW_GAP, CLOUD_Y + SHADOW_GAP, 'rgba(0, 0, 0, 0.7)');
   renderCloud(ctx, CLOUD_X, CLOUD_Y, '#fff');
   renderMessage(ctx);
