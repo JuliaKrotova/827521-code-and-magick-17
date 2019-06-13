0f345caf5152f35cf4d78f65205b394f461f2c9d
JuliaKrotova <julia9867@mail.ru>
2019-06-13 22:07:24 +0700

    Исправление ошибок

--- js/setup.js
+++ js/setup.js
@@ -2,7 +2,7 @@
 
 var NAMES = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
 var SURNAMES = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
-var COUT_COLORS = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
+var COUNT_COLORS = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
 var EYES_COLORS = ['black', 'red', 'blue', 'yellow', 'green'];
 var WIZARDS_COUNT = 4;
 
@@ -29,7 +29,7 @@ var getName = function () {
 var getWizard = function () {
   var wizard = {
     name: getName(),
-    coatColor: getRandomElement(COUT_COLORS),
+    coatColor: getRandomElement(COUNT_COLORS),
     eyesColor: getRandomElement(EYES_COLORS)
   };
   return wizard;
